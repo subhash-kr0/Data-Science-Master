2f6de5513ffbb5b7bf979e26431da3468a460136
Subhash Kumar <subhashkr855@gmail.com>
2024-11-13 21:25:41 +0530

    Module 100

--- Module 78/DiamondPricePrediction2/application.py
+++ Module 78/DiamondPricePrediction2/application.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,render_template,jsonify
 from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
-
+ 
 application=Flask(__name__)
 
 app=application
